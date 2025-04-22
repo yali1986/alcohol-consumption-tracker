17a6e31136baef118332ba547507e2450b8d0e6b
Yalina Gómez <yalinita1985@gmail.com>
2025-04-22 09:46:20 +0200

    title and tailwind installation

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <container className="mx-auto text-center my-14">
+      <h1 className='text-red-500 text-3xl'>Alcohol Consumption tracker</h1> 
+    </container>
+     
+    </>
+  )
+}
+
+export default App
