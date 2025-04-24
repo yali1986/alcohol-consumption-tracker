80ce12ad5c2b43a7c7a5d15a9a6a208b4f256c76
Yalina Gómez <yalinita1985@gmail.com>
2025-04-24 11:38:32 +0200

    modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,8 +25,8 @@ export default function Home() {
    <button className="border border-sky-900 px-4 py-2 rounded-xl mb-2 text-sky-900 text-lg" onClick={handleOpenModal}>Saber más</button>
    </div>
      {showModal && (
-    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
-       <div className="border rounded-xl mb-10 mx-auto p-8 w-6/10 bg-white text-sky-900 text-xl leading-10">
+    <div className="fixed top-[180px] left-0 w-full h-2/3 bg-blue-100 bg-opacity-50 flex justify-center items-center z-50 border border-sky-900 rounded-xl pb-6">
+       <div className="rounded-xl mb-10 mx-auto p-8 w-6/10 bg-white text-sky-900 text-xl leading-10 mt-16">
        <button
               className="absolute top-2 right-4 text-xl font-bold text-sky-900"
               onClick={handleCloseModal}
